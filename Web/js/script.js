2f76ca828455c4b48e26da2d2dcb8bbc33e0ed1d
ntut105650031 <appymhs5049@gmail.com>
2018-06-27 02:27:30 +0800

    add sign in

--- Web/js/script.js
+++ Web/js/script.js
@@ -10,54 +10,113 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
   const $email = $('#email');
-  const $password = $('#password');
+  const $password = $('#password');  
+
   const $btnSignUp = $('#btnSignUp');
   const $btnSignIn = $('#btnSignIn');
   const $btnSignOut = $('#btnSignOut');
+
   const $signInfo = $('#sign-info');
+
   const auth = firebase.auth();
 
-  // SignUp
-  $btnSignUp.click(function(e){
-    const email = $email.val();
-    const pass = $password.val();    
-    // signUp
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(function(e){
-      console.log(e.message);
-      $signInfo.html(e.message);
+  $.fn.showUserData = function(user) {
+    user.providerData.forEach(function (profile) {
+      console.log("Sign-in provider: "+profile.providerId);
+      console.log("  Provider-specific UID: "+profile.uid);
+      console.log("  Name: "+profile.displayName);
+      console.log("  Email: "+profile.email);
+      console.log("  Photo URL: "+profile.photoURL);
     });
+  }
+
+  $.fn.setUsername = function(user){
+    const username = $('#username').val();
+    //const photoURL = $('#photoURL').val();
+    const setDataAction = user.updateProfile({
+      displayName: username
+      //photoURL: photoURL
+    });
+    $.fn.showUserData(user);
+  }
+
+
+  // SignUp
+  $btnSignUp.click(function(){
+    console.log('-1');
+    const username = $('#username').val();
+    const email = $email.val();
+    const password = $password.val();
+    // Sign Up Error
+    if(username==='') {
+      $('#usernameError').html('請填寫暱稱');
+      return;
+    }
+    if(email==='') {
+      $('#emailError').html('請填寫電子郵件位置');
+      return;
+    }
+    if(password==='') {
+      $('#passwordError').html('請填寫密碼');
+      return;
+    }
+    else if(password.length < 8) {
+      $('#passwordError').html('密碼至少要八位數');
+      return;
+    }
+    // signUp
+    console.log('0');
+    const creatAction = auth.createUserWithEmailAndPassword(email, password);
+
+    console.log('1');
+    creatAction.catch(function(error) {
+      console.log(error.message);
+      $signInfo.html(error.message);
+    });
+    console.log('2');
+    creatAction.then(function(){
+      const user = auth.currentUser;
+      $.fn.setUsername(user);
+      alert("註冊成功！");
+      window.location.href = "./index.html";
+    });
+    console.log('3');
+    $.fn.showUserData(auth.currentUser);
   });
 
   // SignIn
-  $btnSignIn.click(function(e){
+  $btnSignIn.click(function(){
+    console.log('btnSignIn');
     const email = $email.val();
-    const pass = $password.val();
+    const password = $password.val();
     // signIn
-    const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(function(e){
-      console.log(e.message);
-      $signInfo.html(e.message);
+    const loginAction = auth.signInWithEmailAndPassword(email, password);
+    console.log('qq');
+    loginAction.catch(function(error){
+      console.log(error.message);
+      $signInfo.html(error.message);
+    });
+    loginAction.then(function(){
+      alert("登入成功！");
+      window.location.href = "./index.html";
     });
   });
+
   // Listening Login User
-  firebase.auth().onAuthStateChanged(function(user){
+  auth.onAuthStateChanged(function(user){
     if(user) {
       console.log(user);
       $signInfo.html(user.email+" is login...");
-      user.providerData.forEach(function (profile) {
-        console.log("Sign-in provider: "+profile.providerId);
-        console.log("  Provider-specific UID: "+profile.uid);
-        console.log("  Name: "+profile.displayName);
-        console.log("  Email: "+profile.email);
-        console.log("  Photo URL: "+profile.photoURL);
-      });
+      $.fn.showUserData(user);
+      
     } else {
-      console.log("not logged in");
+      console.log("listening not logged in");
     }
   });
+
   $btnSignOut.click(function(){
-    firebase.auth().signOut();
+    auth.signOut();
     $signInfo.html('No one login...');
+    alert("登出成功！");
   });
 });
